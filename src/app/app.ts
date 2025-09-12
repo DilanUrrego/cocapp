ee1b63eba0d9c338b86efd9e8815f0ef37cb297c
Dilan <durrego124@gmail.com>
2025-09-12 15:19:09 -0500

    finished features and cta-footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Hero } from './shared/components/hero/hero'
 import { Features } from './shared/components/features/features'
+import { Footer } from './shared/components/footer/footer'
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, Features],
+  imports: [RouterOutlet, Header, Hero, Features, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
