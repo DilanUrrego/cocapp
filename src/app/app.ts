edb7db9808b614aaf7e8115b704059d952a6fd12
Dilan <durrego124@gmail.com>
2025-09-12 11:51:15 -0500

    Hero and Features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { Hero } from './shared/components/hero/hero'
+import { Features } from './shared/components/features/features'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Features],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
